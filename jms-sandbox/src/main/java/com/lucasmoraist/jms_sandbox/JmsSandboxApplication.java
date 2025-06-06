53d090487a68a8fcdd031bc31527bd9073231c8b
lucasmoraist <luksmnt1101@gmail.com>
2025-06-05 21:51:20 -0300

    feat: add email service to send email for client

---
+++ jms-sandbox/src/main/java/com/lucasmoraist/jms_sandbox/JmsSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.lucasmoraist.jms_sandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmsSandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmsSandboxApplication.class, args);
+	}
+
+}
---
+++ jms-sandbox/src/test/java/com/lucasmoraist/jms_sandbox/JmsSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasmoraist.jms_sandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmsSandboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
